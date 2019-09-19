7eadd889a56888959c81d7efa68898276135dd09
GiGodN <TheCaptonNoahJ@gmail.com>
2019-09-19 11:43:34 -0600

    finished smiley Face Lab

--- src/module1/GraphicsLabs/SmileyFace.java
+++ src/module1/GraphicsLabs/SmileyFace.java
@@ -7,11 +7,13 @@ import java.awt.Canvas;
 public class SmileyFace extends Canvas {
 	
 	private static final long serialVersionUID = 4156460185971353879L;
+	
+	public Color gray = new Color(0x333333);
 
 	public SmileyFace() // constructor - sets up the class
 	{
 		setSize(800, 600);
-		setBackground(Color.WHITE);
+		setBackground(gray);
 		setVisible(true);
 	}
 
