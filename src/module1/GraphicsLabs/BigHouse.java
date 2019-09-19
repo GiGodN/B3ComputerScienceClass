6d456e94f9355707b991d34a7c921bc0c10c5f4f
GiGodN <TheCaptonNoahJ@gmail.com>
2019-09-19 13:05:42 -0600

    finished Big house

--- src/module1/GraphicsLabs/BigHouse.java
+++ src/module1/GraphicsLabs/BigHouse.java
@@ -6,12 +6,14 @@ import java.awt.Canvas;
 
 public class BigHouse extends Canvas {
 	
+	public Color gray = new Color(0x333333);
+	
 	private static final long serialVersionUID = 310417227430242327L;
 
 	public BigHouse() // constructor - sets up the class
 	{
 		setSize(800, 600);
-		setBackground(GraphicsRunner.gray);
+		setBackground(gray);
 		setVisible(true);
 	}
 
@@ -21,12 +23,18 @@ public class BigHouse extends Canvas {
 
 	public void bigHouse(Graphics window) {
 		window.setColor(Color.BLUE);
-
 		window.drawString("BIG HOUSE ", 50, 50);
 
-		window.setColor(Color.BLUE);
-
 		window.fillRect(200, 200, 400, 400);
-
+		
+		window.setColor(Color.WHITE);
+		window.fillRect(225, 300, 125, 125);
+		window.fillRect(450, 300, 125, 125);
+		
+		window.setColor(Color.RED);
+		window.fillRect(150, 150, 500, 100);
+		
+		window.setColor(Color.GREEN);
+		window.fillRect(350, 430, 100, 175);
 	}
 }
