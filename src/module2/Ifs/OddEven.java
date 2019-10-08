de1b44baeead250aa6afa39c060b328fc3bc48f9
GiGodN <TheCaptonNoahJ@gmail.com>
2019-10-08 12:29:27 -0600

    Completed Even Check Lab

---
+++ src/module2/Ifs/OddEven.java
@@ -0,0 +1,22 @@
+package module2.Ifs;
+
+public class OddEven {
+	
+	public static void main(String[] args) {
+		System.out.println(isEven(111));
+		System.out.println(isEven(2000));
+		System.out.println(isEven(-99));
+		System.out.println(isEven(1111));
+		System.out.println(isEven(-850));
+	}
+	
+	private static String isEven(int n1) {
+		if (n1 % 2 == 0) {
+			return "" + n1 + " is even";
+		}
+		else {
+			return "" + n1 + " is odd";
+		}
+	}
+
+}
