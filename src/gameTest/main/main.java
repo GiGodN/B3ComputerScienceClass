67c32ada13a83647c820f368c90bdb692a1c96b3
GiGodN <TheCaptonNoahJ@gmail.com>
2019-10-24 13:07:17 -0600

    Started A game test

--- src/gameTest/main/main.java
+++ src/gameTest/main/main.java
@@ -1,8 +1,13 @@
 package gameTest.main;
 
+import gameTest.Engine.io.Window;
+
 public class main implements Runnable{
 	
 	public Thread game;
+	public static Window window;
+	
+	public static final int WIDTH = 1280, HEIGHT = 760;
 	
 	public void start() {
 		game = new Thread(this, "game");
@@ -11,10 +16,24 @@ public class main implements Runnable{
 	
 	public static void init() {
 		System.out.println("initalizing game");
+		window = new Window(WIDTH, HEIGHT, "GAME");
+		window.create();
 	}
 	
 	public void run() {
-		
+		init();
+		while (true) {
+			update();
+			render();
+		}
+	}
+	
+	private void update() {
+		System.out.println("updating game");
+	}
+	
+	private void render() {
+		System.out.println("rendering game");
 	}
 	
 	public static void main(String[] args) {
--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,5 @@
 //Created By Noah John
 module B3ComputerScienceClass {
 	requires java.desktop;
+	requires org.lwjgl.glfw;
 }
