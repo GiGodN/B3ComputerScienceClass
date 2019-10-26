7ddf6397fdfc00500bbc39ab614db2f2b2deb52e
noahm <noahm@DESKTOP-LM8UUKU>
2019-10-25 18:44:13 -0600

    Winodow having issue with GL.createCapabilities will work on fixing
    (Game commit only)

--- src/gameTest/Engine/io/Window.java
+++ src/gameTest/Engine/io/Window.java
@@ -2,6 +2,8 @@ package gameTest.Engine.io;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjglx.debug.opengl.GL;
 
 public class Window {
 
@@ -11,6 +13,10 @@ public class Window {
 	
 	public int frame;
 	public static long time;
+	
+	private Input input;
+	
+	private float backgroundR, backgroundG, backgroundB;
 
 	public Window(int WIDTH, int HEIGHT, String Title) {
 		this.WIDTH = WIDTH;
@@ -24,6 +30,7 @@ public class Window {
 			return;
 		}
 		
+		input = new Input();
 		window = GLFW.glfwCreateWindow(WIDTH, HEIGHT, Title, 0, 0);
 		
 		if (window == 0) {
@@ -33,15 +40,23 @@ public class Window {
 		
 		GLFWVidMode videomode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
 		GLFW.glfwSetWindowPos(window, (videomode.width() - WIDTH) / 2, (videomode.height() - HEIGHT) / 2);
+		
 		GLFW.glfwMakeContextCurrent(window);
-		GLFW.glfwSwapInterval(1);
+		GL.createCapabilities();
+		
+		GLFW.glfwSetKeyCallback(window, input.getKeyboardCallback());
+		GLFW.glfwSetCursorPosCallback(window, input.getMouseMovCallback());
+		GLFW.glfwSetMouseButtonCallback(window, input.getMouseBtnCallback());
 
 		time = System.currentTimeMillis();
 		
 		GLFW.glfwShowWindow(window);
+		GLFW.glfwSwapInterval(1);
 	}
 	
 	public void update() {
+		GL11.glClearColor(backgroundR, backgroundG, backgroundB, 1.0f);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 		GLFW.glfwPollEvents();
 		frame++;
 		if (System.currentTimeMillis() > time + 1000) {
@@ -58,6 +73,19 @@ public class Window {
 	
 	public boolean shouldClose() {
 		return GLFW.glfwWindowShouldClose(window);
+	}
+	
+	public void setBackgroundColor(float r, float g, float b) {
+		backgroundR = r;
+		backgroundG = g;
+		backgroundB = b;
+	}
+	
+	public void destroy() {
+		input.destroy();
+		GLFW.glfwWindowShouldClose(window);
+		GLFW.glfwDestroyWindow(window);
+		GLFW.glfwTerminate();
 	}
 
 }
--- src/gameTest/main/Main.java
+++ src/gameTest/main/Main.java
@@ -1,8 +1,11 @@
 package gameTest.main;
 
+import org.lwjgl.glfw.GLFW;
+
+import gameTest.Engine.io.Input;
 import gameTest.Engine.io.Window;
 
-public class main implements Runnable{
+public class Main implements Runnable{
 	
 	public Thread game;
 	public Window window;
@@ -17,6 +20,7 @@ public class main implements Runnable{
 	public void init() {
 		System.out.println("initalizing game");
 		window = new Window(WIDTH, HEIGHT, "GAME");
+		window.setBackgroundColor(1.0f, 0f, 0f);
 		window.create();
 	}
 	
@@ -25,11 +29,16 @@ public class main implements Runnable{
 		while (!window.shouldClose()) {
 			update();
 			render();
+			if(Input.isKeyDown(GLFW.GLFW_KEY_ESCAPE))
+				return;
 		}
+		window.destroy();
 	}
 	
 	private void update() {
 		window.update();
+		if(Input.isBtnDown(GLFW.GLFW_MOUSE_BUTTON_LEFT))
+			System.out.println("x: " + Input.getMouseX() + ", y: " + Input.getMouseY());
 	}
 	
 	private void render() {
@@ -37,7 +46,7 @@ public class main implements Runnable{
 	}
 	
 	public static void main(String[] args) {
-		new main().start();
+		new Main().start();
 	}
 	
 }
