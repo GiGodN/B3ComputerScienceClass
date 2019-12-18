220f28a05045be1d6456321813eb9786a95b8bf1
GiGodN <TheCaptonNoahJ@gmail.com>
2019-12-18 12:01:10 -0700

    idk why it wants me to commit this

--- src/module4/classess1/CookieJarRunner.java
+++ src/module4/classess1/CookieJarRunner.java
@@ -1,18 +1,16 @@
 package module4.classess1;
 
-public class CookieJarRunner
-{
-	public static void main( String[] args )
-	{
-		CookieJar c = new CookieJar( 3000 );
-		System.out.println( c.isFull( ) );	
-		c.addStuff( 500 );
-		System.out.println( c.isFull( ) );
-		System.out.println( c.addStuff( 2000 ) );
-		System.out.println( c.spaceLeft( ) );
-		System.out.println( c.isFull( ) );	
-		System.out.println( c.addStuff( 2000 ) );
-		System.out.println( c.spaceLeft( ) );
-		System.out.println( c.isFull( ) );				
+public class CookieJarRunner {
+	public static void main(String[] args) {
+		CookieJar c = new CookieJar(3000);
+		System.out.println(c.isFull());
+		c.addStuff(500);
+		System.out.println(c.isFull());
+		System.out.println(c.addStuff(2000));
+		System.out.println(c.spaceLeft());
+		System.out.println(c.isFull());
+		System.out.println(c.addStuff(2000));
+		System.out.println(c.spaceLeft());
+		System.out.println(c.isFull());
 	}
 }
