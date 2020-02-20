5a2841934d08c639a52f7cfabe218eac60de6bb9
user03 <user03@gigodn-VirtualBox>
2020-02-20 12:22:14 -0700

    fixed problem

--- src/module6/arrayList3/Word.java
+++ src/module6/arrayList3/Word.java
@@ -33,4 +33,19 @@ public class Word {
 	public String toString() {
 		return word;
 	}
+	
+	public static void main(String[] args) {
+		Word one = new Word("chicken");
+		out.println(one);
+		out.println("num vowels == " + one.getNumVowels());
+		out.println("num chars == " + one.getLength());
+		one.setWord("alligator");
+		out.println("\n\n" + one);
+		out.println("num vowels == " + one.getNumVowels());
+		out.println("num chars == " + one.getLength());
+		one.setWord("elephant");
+		out.println("\n\n" + one);
+		out.println("num vowels == " + one.getNumVowels());
+		out.println("num chars == " + one.getLength());
+	}
 }
